59c0b71176120013bbf881ab70d11fa2d893d4b4
Vardhman Mehta <mehtav@cs.washington.edu>
2016-03-24 02:08:08 -0500

    need to add written testimonials

--- myproject/myproject/myapp/admin.py
+++ myproject/myproject/myapp/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from myproject.myapp.models import PackageImages, SliderImages, Package, TestimonialVideo
 
-admin.site.register(PackageImages)
-admin.site.register(SliderImages)
-admin.site.register(Package)
+
+
+class PackageInline(admin.StackedInline):
+	model = Package
+
+
+class PackageImagesAdmin(admin.ModelAdmin):
+	list_display = ('image_text', 'image_link')
+	inlines=[PackageInline]
+
+admin.site.register(PackageImages, PackageImagesAdmin)
+
+class SliderImagesAdmin(admin.ModelAdmin):
+	list_display = ('image_caption', 'slider_link')
+
+
+admin.site.register(SliderImages, SliderImagesAdmin)
 admin.site.register(TestimonialVideo)
 
 
